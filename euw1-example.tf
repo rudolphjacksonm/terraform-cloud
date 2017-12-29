e3bb1a217d37c22a01e21959d015f644ef38ae3a
Jack Morris <rudolphjacksonm@gmail.com>
2017-12-29 12:34:23 +0000

    create vpc and subnets, remove test instance

--- euw1-example.tf
+++ euw1-example.tf
@@ -1,8 +1,43 @@
 provider "aws" {
-  region     = "eu-west-1"
+  region     = "${var.region}"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0fac5276"
-  instance_type = "t2.micro"
+resource "aws_vpc" "main" {
+  cidr_block = "${var.vpc_cidr}"
+}
+
+resource "aws_subnet" "public-1a" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "1a"
+}
+
+resource "aws_subnet" "public-1b" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "1b"
+}
+
+resource "aws_subnet" "public-1c" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "1c"
+}
+
+resource "aws_subnet" "private-1a" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "1a"
+}
+
+resource "aws_subnet" "private-1b" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "1b"
+}
+
+resource "aws_subnet" "private-1c" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.0.6.0/24"
+  availability_zone = "1c"
 }
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+    default = "eu-west-1"
+}
+
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
