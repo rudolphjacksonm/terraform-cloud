77ff0a7af37956def2941dbdb49ce4fc34338524
Jack Morris <rudolphjacksonm@gmail.com>
2017-12-29 12:54:24 +0000

    add internet gateway

--- euw1-example.tf
+++ euw1-example.tf
@@ -6,6 +6,14 @@ resource "aws_vpc" "main" {
   cidr_block = "${var.vpc_cidr}"
 }
 
+resource "aws_internet_gateway" "vpc" {
+  vpc_id = "${aws_vpc.main.id}"
+  
+  tags = {
+    Name = "main-IGW"
+  }
+}
+
 resource "aws_subnet" "public-1a" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.1.0/24"
