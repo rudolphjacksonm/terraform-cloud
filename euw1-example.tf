86d9b9629364d847bc388710d7c6d7f61440006e
Jack Morris <rudolphjacksonm@gmail.com>
2017-12-29 12:37:21 +0000

    correct AZs

--- euw1-example.tf
+++ euw1-example.tf
@@ -9,35 +9,35 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public-1a" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.1.0/24"
-  availability_zone = "1a"
+  availability_zone = "eu-west-1a"
 }
 
 resource "aws_subnet" "public-1b" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.2.0/24"
-  availability_zone = "1b"
+  availability_zone = "eu-west-1b"
 }
 
 resource "aws_subnet" "public-1c" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.3.0/24"
-  availability_zone = "1c"
+  availability_zone = "eu-west-1c"
 }
 
 resource "aws_subnet" "private-1a" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.4.0/24"
-  availability_zone = "1a"
+  availability_zone = "eu-west-1a"
 }
 
 resource "aws_subnet" "private-1b" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.5.0/24"
-  availability_zone = "1b"
+  availability_zone = "eu-west-1b"
 }
 
 resource "aws_subnet" "private-1c" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.0.6.0/24"
-  availability_zone = "1c"
+  availability_zone = "eu-west-1c"
 }
